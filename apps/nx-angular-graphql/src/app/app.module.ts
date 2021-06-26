d5fbce5850ee42255d3e7b5ef87d566cb8051411
AndrewJBateman <gomezbateman@yahoo.com>
2021-06-26 23:39:19 +0200

    fix: update imports due to error from dep upgrades

--- apps/nx-angular-graphql/src/app/app.module.ts
+++ apps/nx-angular-graphql/src/app/app.module.ts
@@ -1,12 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FeatureTasksModule } from '@nx-angular-graphql/feature-tasks';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { GraphQLModule } from './graphql.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, GraphQLModule, FeatureTasksModule],
   providers: [],
   bootstrap: [AppComponent],
 })
